2cd7f0280de8ad1d518693dc783ac0d7827e24a6
mikebos <mikebos@mik3.com>
2015-12-14 12:20:58 +0100

    better printed readability added command line parameter

--- ExactPrint/Form1.cs
+++ ExactPrint/Form1.cs
@@ -9,7 +9,7 @@ using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
-using Microsoft.Office.Interop.Word; 
+using Microsoft.Office.Interop.Word;
 
 namespace ExactPrint
 {
@@ -22,47 +22,74 @@ namespace ExactPrint
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            Convert();
+        }
+
+        private void Convert()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            string fileToConvert;
+            if (args.Length == 1)
             {
-                string text = File.ReadAllText(openFileDialog1.FileName);
-                text = Regex.Replace(text, @"[^\u0009-\u0010|^\u0020-\u007E]", string.Empty);
-  
-                object missing = System.Reflection.Missing.Value;
-                object Visible=true;
-                object start1 = 0;
-                object end1 = 0;
-
-                Microsoft.Office.Interop.Word.Application WordApp = new Microsoft.Office.Interop.Word.Application();
-                Document adoc = WordApp.Documents.Add(ref missing, ref missing, ref missing, ref missing);
-                Range rng = adoc.Range(ref start1, ref missing);
-
- 
-
-                try
-                {              
-                    rng.Font.Size = 5;
-                    rng.Font.Name = "Courier New";
-                    // rng.PageSetup.Orientation = WdOrientation.wdOrientLandscape;
-                    rng.InsertAfter(text);
-                    Microsoft.Office.Interop.Word.Paragraphs paragraphs = adoc.Paragraphs;
-                    foreach (Microsoft.Office.Interop.Word.Paragraph paragraph in paragraphs)
-                    {
-                        if (paragraph.Range.Text.Trim() == string.Empty)
-                        {
-                            paragraph.Range.Select();
-                            WordApp.Selection.Delete();
-                        }
-                    } 
-                    WordApp.Visible = true;
-                }
-
-                catch (Exception ex)
+                if (openFileDialog1.ShowDialog() != DialogResult.OK)
                 {
-                    MessageBox.Show(ex.Message);
-                }        
+                    System.Windows.Forms.Application.Exit();
+                    return;
+                }
+                else
+                {
+                    fileToConvert = openFileDialog1.FileName;
+                }
+            }
+            else if (!File.Exists(args[1]))
+            {
+                MessageBox.Show("Not a valid file.", "Error");
+                System.Windows.Forms.Application.Exit();
+                return;
+            } else
+            {
+                fileToConvert = args[1];
+            }
+
+
+            string text = File.ReadAllText(fileToConvert);
+            text = Regex.Replace(text, @"[^\u0009-\u0010|^\u0020-\u007E]", string.Empty);
+
+            object missing = System.Reflection.Missing.Value;
+            object Visible = true;
+            object start1 = 0;
+            object end1 = 0;
+
+            Microsoft.Office.Interop.Word.Application WordApp = new Microsoft.Office.Interop.Word.Application();
+            Document adoc = WordApp.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+            Range rng = adoc.Range(ref start1, ref missing);
+
+            try
+            {
+                rng.PageSetup.PaperSize = WdPaperSize.wdPaperA4;
+                rng.Font.Size = 5;
+                rng.Font.Bold = 1;
+                rng.Font.Name = "Courier New";
+                // rng.PageSetup.Orientation = WdOrientation.wdOrientLandscape;
+                rng.InsertAfter(text);
+                Microsoft.Office.Interop.Word.Paragraphs paragraphs = adoc.Paragraphs;
+                foreach (Microsoft.Office.Interop.Word.Paragraph paragraph in paragraphs)
+                {
+                    if (paragraph.Range.Text.Trim() == string.Empty)
+                    {
+                        paragraph.Range.Select();
+                        WordApp.Selection.Delete();
+                    }
+                }
+                WordApp.Visible = true;
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
             System.Windows.Forms.Application.Exit();
-            
         }
     }
 }
+
